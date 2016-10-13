f3b33932e4451bef959e79b3d34fd3dd598ef244
Colin MacKenzie <guru-florida@users.noreply.github.com>
2016-10-12 21:09:36 -0400

    button groups now created via band scale

--- web-ui/js/button-indicator.js
+++ web-ui/js/button-indicator.js
@@ -66,19 +66,23 @@ ButtonGroupIndicator.prototype.attach = function(lane)
     if(this.options.buttons.length==0)
         return false;
 
-    var button_width;
-    if(this.options.button_options.width == null) {
-        // buttons are evenly spaced within the group control lane range
-        button_width = (this.arcrange[1]-this.arcrange[0]) / this.options.buttons.length;
-    }
 
-    var offset = this.arcrange[0];
+    // this band scale computes the radial range of each button in the group
+    var band = d3.scaleBand()
+        .domain(this.options.buttons)
+        .range(this.scale.range())
+        .paddingInner(0.05);
+    var bandwidth = band.bandwidth();
+
+    // create each button
     $.each(this.options.buttons, function(k, b) {
         var name = (b && b.name) ? b.name : "button"+k;
-        var options = $.extend(true, {}, _this.options.button_options);
 
-        if(!options.lane)
-            options.lane = $.extend({}, _this.options.lane);
+        // merge the options for the button
+        var options = $.extend(true, {
+            lane: _this.options.lane,
+            arcrange: [ band(b), band(b)+bandwidth ]
+        }, _this.options.button_options);
 
         // the user can specify buttons as a simple label list, object list or actual button references
         var button;
@@ -92,12 +96,7 @@ ButtonGroupIndicator.prototype.attach = function(lane)
             button = new ButtonIndicator(options);
             if(b.id) name = b.id;
             button.caption = b.caption;
-            console.log(button);
         }
-
-        //options.lane.arcrange = [ offset, offset + button_width ];
-        button.options.arcrange = [ offset, offset + 0.98*button_width ];
-        offset += button_width;
 
         lane.dial.plugin(name, button);
     });
